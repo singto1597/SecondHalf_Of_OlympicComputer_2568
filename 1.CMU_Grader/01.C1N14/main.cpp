8549a9aa666808b89f359bd18cc0dbb731926325
Phatthanaphon Sutham <singto1597@gmail.com>
2025-10-11 23:01:37 +0700

    Do one question

---
+++ 1.CMU_Grader/01.C1N14/main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main (){
+    char inputString[3000] = "";
+    cin >> inputString;
+    int i = 0;
+
+    char *firstIndexPointer = inputString;
+
+    while (*firstIndexPointer != '\0'){
+        if (i % 2 == 0){
+            *firstIndexPointer = toupper(*firstIndexPointer);
+        }
+        else {
+            *firstIndexPointer = tolower(*firstIndexPointer);
+        }
+        firstIndexPointer++;
+        i++;
+    }
+
+    cout << inputString << endl;
+
+
+}
