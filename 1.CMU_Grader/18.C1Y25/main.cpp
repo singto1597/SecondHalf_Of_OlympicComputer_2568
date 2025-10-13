a7309a29fada3db776eeb573fa6da876822d700b
singto_Ubuntu <singto1597@gmail.com>
2025-10-13 17:55:41 +0700

    Do more

--- 1.CMU_Grader/18.C1Y25/main.cpp
+++ 1.CMU_Grader/18.C1Y25/main.cpp
@@ -2,6 +2,24 @@
 using namespace std;
 
 int main (){
-    int numberA;
+    int numberOfList;
+    int numberOfTarget;
+    cin >> numberOfList >> numberOfTarget;
+    int listOfNumber[numberOfList] = {};
+    for (int i = 0; i < numberOfList; i++){
+        int buffer;
+        cin >> buffer;
+        listOfNumber[i] = buffer;
+    }
+    sort(listOfNumber, listOfNumber + numberOfList);
+
+    int index = 0;
+    
+    while (index < numberOfTarget){
+        int target;
+        cin >> target;
+        cout << upper_bound(listOfNumber, listOfNumber + numberOfList, target) - listOfNumber << endl;
+        index++;
+    }
 
 }
